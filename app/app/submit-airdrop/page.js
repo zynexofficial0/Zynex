da6571c1b90bcd8c31893541e11891890fe59676
zynexofficial0 <zynexofficial09@gmail.com>
2026-04-21 10:37:42 +0500

    Create SubmitAirdrop component for airdrop submission

---
+++ app/app/submit-airdrop/page.js
@@ -0,0 +1,8 @@
+export default function SubmitAirdrop() {
+  return (
+    <div style={{ padding: "40px", color: "white", background: "black", minHeight: "100vh" }}>
+      <h1>Submit Airdrop</h1>
+      <p>Your submit airdrop page is working 🚀</p>
+    </div>
+  )
+}
